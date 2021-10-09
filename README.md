a6e19f678f7b1ba27bf359bf4b4cef851acbfd08
glori4n <glori4n@gmail.com>
2021-10-09 18:08:23 -0300

    09/10/2021 - Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+<p align="center"><img src="https://github.com/glori4n/laravel-relationships-exercise/blob/master/public/images/glogo.png"></>
+
+# laravel-phpunit
+This project is an exercise exploring Laravel's PHPUnit concepts and types of interactions as well as some form of TDD.
---
+++ tests/Feature/BasicTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Box;
+
+class BasicTest extends TestCase
+{
+    # Test function for Box class
+    public function testBoxContents()
+    {
+        $box = new Box(['toy']);
+        $this->assertTrue($box->has('toy'));
+        $this->assertFalse($box->has('ball'));
+    }
+
+    public function testTakeOneFromTheBox()
+    {
+        $box = new Box(['torch']);
+        $this->assertEquals('torch', $box->takeOne());
+
+        // Null, now the box is empty
+        $this->assertNull($box->takeOne());
+    }
+
+    // These assertions works with arrays.
+    public function testStartsWithALetter()
+    {
+        $box = new Box(['toy', 'torch', 'ball', 'cat', 'tissue']);
+
+        $results = $box->startsWith('t');
+
+        $this->assertCount(3, $results);
+        $this->assertContains('toy', $results);
+        $this->assertContains('torch', $results);
+        $this->assertContains('tissue', $results);
+
+        // Empty array if passed even
+        $this->assertEmpty($box->startsWith('s'));
+    }
+}
